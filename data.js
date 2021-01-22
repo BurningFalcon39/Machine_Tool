2ad4a450c2bb9ef2c09fa191318c7601258ea475
BurningFalcon39 <thomas.fleming@stu.henderson.kyschools.us>
2021-01-22 12:52:19 -0600

    It's Machine Tool

---
+++ data.js
@@ -0,0 +1,85 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-machinetool",
+      "name": "MachineTool",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 0.16584543111586036,
+        "pitch": 0.08704878959052031,
+        "fov": 1.3715802068843215
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": -0.19035447726703936,
+          "pitch": -0.034903398064717095,
+          "title": "MachineIntro",
+          "text": "Text"
+        },
+        {
+          "yaw": 0.7345303796634628,
+          "pitch": -0.025964144717455895,
+          "title": "MachineTool1",
+          "text": "Text"
+        },
+        {
+          "yaw": 1.6129708191491723,
+          "pitch": -0.0218801308291372,
+          "title": "MachineTool2",
+          "text": "Text"
+        },
+        {
+          "yaw": 2.4008304031977676,
+          "pitch": -0.025503128683940446,
+          "title": "MachineTool3",
+          "text": "Text"
+        },
+        {
+          "yaw": -3.133544361125942,
+          "pitch": -0.01588818951802118,
+          "title": "MachineTool4",
+          "text": "Text"
+        },
+        {
+          "yaw": -2.366820671959271,
+          "pitch": -0.0031578240636864763,
+          "title": "MachineTool5",
+          "text": "Text"
+        },
+        {
+          "yaw": -1.444336763572199,
+          "pitch": 0.007413365413338369,
+          "title": "MachineTool6",
+          "text": "Text"
+        }
+      ]
+    }
+  ],
+  "name": "MachineTool",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
